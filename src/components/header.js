dd3bed261c9a7a804ac90508a929785474d55e0d
Algy Taylor <Algy.Taylor@ibm.com>
2021-04-05 18:35:09 +0100

    feat: remove gatsby branding from layout

--- src/components/header.js
+++ src/components/header.js
@@ -1,22 +1,25 @@
 import * as React from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
+import styled from "styled-components"
+
+const Wrapper = styled.header`
+  background: darkgreen;
+  margin-bottom: 1.45rem;
+`;
+const InnerWrapper = styled.div`
+  margin: 0 auto;
+  max-width: 960;
+  padding: 1.45rem 1.0875rem;
+`;
+const Text = styled.h1`
+  margin: 0;
+`;
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
+  <Wrapper>
+    <InnerWrapper>
+      <Text>
         <Link
           to="/"
           style={{
@@ -26,9 +29,9 @@ const Header = ({ siteTitle }) => (
         >
           {siteTitle}
         </Link>
-      </h1>
-    </div>
-  </header>
+      </Text>
+    </InnerWrapper>
+  </Wrapper>
 )
 
 Header.propTypes = {
