251c48b300eb6d9f111f21f4ca3031b60ddf7a09
Algy Taylor <Algy.Taylor@ibm.com>
2021-04-05 18:35:36 +0100

    feat: add basic details to index page
    Add some basic wedding details to the index page and remove the last of the Gatsby branding.

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,17 +8,14 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
+    <h1>Important Details</h1>
+    <p>
+      Our wedding is on <strong>22<sup>nd</sup> October 2022</strong>. The
+      main service will be held at <strong>2pm</strong> in <strong>St.
+      Giles' Parish Church</strong> in Wrexham. Celebrations will continue
+      from <strong>6pm</strong> at the <strong>Ramada Plaza</strong> hotel,
+      also in Wrexham.
+    </p>
     <p>
       <Link to="/page-2/">Go to page 2</Link> <br />
       <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
